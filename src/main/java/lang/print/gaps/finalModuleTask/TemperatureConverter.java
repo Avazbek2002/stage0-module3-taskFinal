bf6b32eb7d330cb69477c8fd910327b92c225b95
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-07-26 03:06:10 +0500

    Final task

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        System.out.println((float) 9 * temperatureCelsius/5 + 32);
     }
 }
